50a3739d9d9e862b94f016aadd8d7005425948be
Imon Ovbude <imonovbude@gmail.com>
2019-01-09 15:09:14 -0800

    mapped over todos array of objects and printed each value to the screen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
 
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
 const todos = [
-  { todo: 'Walk dog' }
+  { todo: 'Walk dog' },
+  { todo: 'Wash car' }
 ]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+
+    this.state = {
+      todoList: todos
+    }
+    // console.log(this.state.todoList)
+  }
+
   render() {
     return (
-      <div>
 
-      </div>
+      < div >
+        <TodoList todosList={this.state.todoList} />
+        <TodoForm />
+      </div >
+
     );
   }
+
 }
 
 export default App;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+
+const TodoList = (props) => {
+    return (
+        <div>
+            {props.todosList.map((todo, index) => {
+                console.log(todo)
+                return <div key={index}>{todo.todo}</div>
+            })}
+        </div>
+    );
+}
+
+export default TodoList;
